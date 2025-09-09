a576b2149eb880a321d6e9addab089f74cc3f6d1
VAHAB <vahab.azimzadeh@gmail.com>
2025-09-09 20:36:31 +0200

    prettier

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,11 +1,11 @@
 export default function robots() {
-  const base = 'https://vahab-amz.com';
-  return {
-    rules: [
-      { userAgent: '*', allow: '/' },
-      { userAgent: '*', disallow: ['/api/'] },
-    ],
-    sitemap: `${base}/sitemap.xml`,
-    host: base
-  };
+    const base = 'https://vahab-amz.com';
+    return {
+        rules: [
+            { userAgent: '*', allow: '/' },
+            { userAgent: '*', disallow: ['/api/'] },
+        ],
+        sitemap: `${base}/sitemap.xml`,
+        host: base,
+    };
 }
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,24 @@
 export default function sitemap() {
-  const base = 'https://vahab-amz.com';
-  const locales = ['en', 'es'];
-  const routes = ['', '/about', '/cv'];
+    const base = 'https://vahab-amz.com';
+    const locales = ['en', 'es'];
+    const routes = ['', '/about', '/cv'];
 
-  const urls = [];
+    const urls = [];
 
-  for (const locale of locales) {
-    for (const route of routes) {
-      urls.push({
-        url: `${base}/${locale}${route}`,
-        lastModified: new Date(),
-        alternates: {
-          languages: {
-            en: `${base}/en${route}`,
-            es: `${base}/es${route}`
-          }
+    for (const locale of locales) {
+        for (const route of routes) {
+            urls.push({
+                url: `${base}/${locale}${route}`,
+                lastModified: new Date(),
+                alternates: {
+                    languages: {
+                        en: `${base}/en${route}`,
+                        es: `${base}/es${route}`,
+                    },
+                },
+            });
         }
-      });
     }
-  }
 
-  return urls;
+    return urls;
 }
