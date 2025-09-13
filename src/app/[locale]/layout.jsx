66e57f369eb362efa127047ce987028123cd1358
VAHAB <vahab.azimzadeh@gmail.com>
2025-09-13 13:38:53 +0200

    update default theme to dark in RootLayout component

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -30,7 +30,7 @@ export default async function RootLayout({ children, params }) {
                 <NextIntlClientProvider>
                     <ThemeProvider
                         attribute="class"
-                        defaultTheme="system"
+                        defaultTheme="dark"
                         enableSystem
                         disableTransitionOnChang
                     >
